01f8e3d81c239fa0feee70e46658ae8cbb00206e
Brunno Bettini <brunnobb@gmail.com>
2016-01-29 22:42:18 -0200

    Commit teste geral Web TEX

---
+++ js/users.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('#example').DataTable();
+} );
+
+function showConfirm(){
+	 $('#myModal').modal();
+}
+
+function gotoEdit(){
+	document.location.href = "form.html";
+}
